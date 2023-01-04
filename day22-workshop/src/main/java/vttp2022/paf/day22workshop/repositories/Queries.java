7449686b308a490260dbb4bc6e6f1ad63fa38ac7
cwslinda <lindacheung94@gmail.com>
2023-01-05 00:39:07 +0800

    completed

--- day22-workshop/src/main/java/vttp2022/paf/day22workshop/repositories/Queries.java
+++ day22-workshop/src/main/java/vttp2022/paf/day22workshop/repositories/Queries.java
@@ -1,18 +1,21 @@
 package vttp2022.paf.day22workshop.repositories;
 
-public class Queries {
 
+public class Queries {
+    
     public static final String SQL_SELECT_RSVPS = "select * from rsvp";
 
-    public static final String SQL_SELECT_RSVP_BY_NAME = "select * from rsvp where guest_name like ?";
+    public static final String SQL_SELECT_RSVP_BY_NAME = "select * from rsvp where name like ?";
 
     public static final String SQL_SELECT_RSVP_BY_EMAIL = "select * from rsvp where email like ?";
+    
+    public static final String SQL_INSERT_RSVP = 
+        "insert into rsvp(name, email, phone, confirmation_date, comments)" +
+        "values(?, ?, ?, ?, ?)";
 
-    public static final String SQL_INSERT_NEW_RSVP = 
-        "insert into rsvp(guest_name, email, phone, confirmation_date, comments)" +
-        "values(?,?,?,?,?)";
+    public static final String SQL_UPDATE_RSVP_BY_EMAIL = 
+        "update rsvp set name = ?, phone = ? , confirmation_date = ?, comments = ? where email = ?";
 
-    public static final String SQL_UPDATE_RSVP = 
-        "update rsvp set guest_name = ?, phone = ?, confirmation_date = ?, comments = ? where email = ?";
-
+    public static final String SQL_COUNT_RSVPS = 
+        "select count(*) as total from rsvp";
 }
--- day22-workshop/src/main/java/vttp2022/paf/day22workshop/repositories/RSVPRepo.java
+++ day22-workshop/src/main/java/vttp2022/paf/day22workshop/repositories/RSVPRepo.java
@@ -1,14 +1,11 @@
 package vttp2022.paf.day22workshop.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import vttp2022.paf.day22workshop.models.RSVP;
-
 import static vttp2022.paf.day22workshop.repositories.Queries.*;
 
 import java.util.LinkedList;
@@ -20,28 +17,11 @@ public class RSVPRepo {
     @Autowired
     private JdbcTemplate template;
 
-    public List<RSVP> getAllRsvp(){
+    public List<RSVP> queryAllRSVPs() {
+        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVPS);
 
-        final List<RSVP> rsvp = new LinkedList<>();
-
-        final SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVPS);
-
-        while(rs.next()){
-            rsvp.add(RSVP.create(rs));
-        }
-
-        return rsvp;
-
-
-    }
-
-    public List<RSVP> queryRSVPByName(String name){
-        
         final List<RSVP> rsvps = new LinkedList<>();
-
-        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVP_BY_NAME, "%%%s%%".formatted(name));
-        
-        while(rs.next()){
+        while (rs.next()) {
             RSVP rsvp = RSVP.create(rs);
             rsvps.add(rsvp);
         }
@@ -49,18 +29,52 @@ public class RSVPRepo {
         return rsvps;
     }
 
-    // add new data into the database, check if there is an existing record already
-    public boolean insertRSVP(RSVP rsvp){
-        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVP_BY_EMAIL, rsvp.getEmail());
+    public List<RSVP> queryRSVPByName(String name) {
+        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVP_BY_NAME, "%%%s%%".formatted(name));
 
-        if(rs.next()){
-            return updateRSVP(rsvp);
-        } else {
-            return template.update(SQL_INSERT_NEW_RSVP, rsvp.getName(), rsvp.getEmail(), rsvp.getPhone(), rsvp.getConfirmationDate(), rsvp.getComments()) > 0;
+        final List<RSVP> rsvps = new LinkedList<>();
+        while (rs.next()) {
+            RSVP rsvp = RSVP.create(rs);
+            rsvps.add(rsvp);
         }
+
+        return rsvps;
     }
 
-    public boolean updateRSVP(RSVP rsvp){
-        return template.update(SQL_UPDATE_RSVP, rsvp.getName(), rsvp.getPhone(), rsvp.getConfirmationDate(), rsvp.getComments(), rsvp.getEmail()) > 0;
+    public boolean insertRSVP(RSVP rsvp) {
+        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVP_BY_EMAIL, rsvp.getEmail());
+
+        if (rs.next()) {
+            return updateRSVP(rsvp);
+        } else {
+            return template.update(SQL_INSERT_RSVP, rsvp.getName(), rsvp.getEmail(), rsvp.getPhone(), rsvp.getConfirmationDate(), rsvp.getComments()) > 0;
+        }
+
+    }
+
+    public RSVP queryRSVPByEmail(String email) {
+        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_RSVP_BY_EMAIL, email);
+
+        final List<RSVP> rsvps = new LinkedList<>();
+        while (rs.next()) {
+            RSVP rsvp = RSVP.create(rs);
+            rsvps.add(rsvp);
+        }
+
+        return rsvps.get(0);
+    }
+
+    public boolean updateRSVP(RSVP rsvp) {
+        return template.update(SQL_UPDATE_RSVP_BY_EMAIL, rsvp.getName(), rsvp.getPhone(), rsvp.getConfirmationDate(), rsvp.getComments(), rsvp.getEmail()) > 0;
+    }
+
+    public Integer queryRSVPCount() {
+        SqlRowSet rs = template.queryForRowSet(SQL_COUNT_RSVPS);
+        Integer count = 0;
+        while (rs.next()) {
+            count = rs.getInt("total");
+        }
+
+        return count;
     }
 }
